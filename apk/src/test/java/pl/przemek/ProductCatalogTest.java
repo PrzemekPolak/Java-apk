744fa67d5ea634def74705d5e85f94ba6cb2795d
s-013-19 <s-013-19@gumiforest.ae.krakow.pl>
2021-11-19 14:34:49 +0100

    zajecia 19-11

---
+++ apk/src/test/java/pl/przemek/ProductCatalogTest.java
@@ -0,0 +1,11 @@
+package pl.przemek;
+
+import org.junit.jupiter.api.Test;
+
+public class ProductCatalogTest {
+
+    @Test
+    void itAllowAddProductToCatalog() {
+        ProductCatalog catalog = thereIsProductCatalog();
+    }
+}
